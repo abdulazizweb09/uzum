1d74ef84c078ab2ee672366aade2ae415e22dc4a
abdulazizweb09 <xakimovxadyatillo92@gmail.com>
2025-06-12 08:07:32 +0500

    initial commit

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div>{children}</div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
